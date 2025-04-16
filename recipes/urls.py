409d6a0e60b55b60949aaf102cdaadd3ee010e6b
Alberto Fernando de Moura <albertofmoura@yahoo.com.br>
2025-04-15 22:46:50 -0300

    separando html

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from recipes.views import home, contato, sobre
+from recipes.views import home
 
 urlpatterns = [
     path("", home), 
-    path("contato/", contato),
-    path("sobre/", sobre),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Estamos na Home!")
-
-
-def contato(request):
-    return HttpResponse("Estamos em Contato!")
-
-
-def sobre(request):
-    return HttpResponse("Estamos em Sobre!")
+    return render(request, 'recipes\home.html', context={"name":"Receitas Alberto"}
+                  )
